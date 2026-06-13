e056ba09d4fc652bfed15d8d1e9e1e09c12d0050
Favio <favio.ramos.yupanqui@gmail.com>
2026-06-12 22:14:16 -0500

    Agregar logo y actualizar navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,10 +8,11 @@ export default function Navbar() {
       <Link href="/" className="flex items-center gap-3 font-bold text-2xl text-indigo-700 tracking-tight">
         {/* Aquí insertamos el logo */}
         <Image 
-          src="logo-catacaos.png" 
+          src="/logo-catacaos.png" 
           alt="Logo Club Catacaos" 
           width={40} 
           height={40} 
+          unoptimized
           className="object-contain"
         />
         Club Catacaos
